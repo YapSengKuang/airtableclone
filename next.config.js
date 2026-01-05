2ca6bd9573825e136987ffc284333e0c86743869
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-05 13:38:22 +1100

    altered next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  */
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     typedRoutes: false,
   },
 };
 
-module.exports = nextConfig;
-
-
-export default config;
-
+export default nextConfig;
