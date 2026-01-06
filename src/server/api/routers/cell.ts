baad95886599c7caaf07ab74038fafb874607634
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-06 23:28:31 +1100

    ples work

--- src/server/api/routers/cell.ts
+++ src/server/api/routers/cell.ts
@@ -2,6 +2,18 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 
+// JSON-safe Zod schema 
+const json: z.ZodType<unknown> = z.lazy(() =>
+    z.union([ 
+        z.string(), 
+        z.number(), 
+        z.boolean(), 
+        z.null(), 
+        z.array(json), 
+        z.record(json), 
+    ]) 
+);
+
 export const cellRouter = createTRPCRouter({
   update: protectedProcedure
     .input(
