b9158a332a18ad8474f5410feaeb62712840dae7
Seng Kuang Yap <syap0014@student.monash.edu>
2026-01-06 23:48:21 +1100

    added code to middleware to always run

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware(() => {
+    console.log('Clerk middleware executed');
+    return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
